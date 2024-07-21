8857bd8929898e57471f061376fd6cca825c498d
EmilGlz <emilquluzade2001@gmail.com>
2024-07-21 19:17:07 -0400

    Added sidebar in navbar to be responsive

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,6 +1,8 @@
+'use client'
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./navbar.module.css";
+import Image from "next/image";
 
 const links = [
   {
@@ -21,6 +23,12 @@ const links = [
 ];
 
 const Navbar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.logo}>
@@ -37,6 +45,16 @@ const Navbar = () => {
       <div className={styles.rightBox}>
         <span className={styles.rightBoxText}>Let&apos;s Talk</span>
         <img alt="" src="/lets-talk.png" className={styles.rightBoxIcon} />
+      </div>
+      <div className={`${styles.sidebarButton} fixed right-0`} onClick={toggleSidebar}>
+        <Image width={50} height={50} src={isSidebarOpen ? "close.svg" : "menu.svg"} alt={"open-close-sidebar"} />
+      </div>
+      <div className={`${styles.sidebar} ${isSidebarOpen ? styles.visible : styles.hidden}`}>
+        {links.map((link) => (
+          <Link key={link.id} href={link.url} className={styles.link}>
+            {link.title}
+          </Link>
+        ))}
       </div>
     </div>
   );
