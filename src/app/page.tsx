64025d07ef0e39231b9658b92df290db907ae097
EmilGlz <58591330+EmilGlz@users.noreply.github.com>
2024-07-23 16:22:07 -0400

    Added new blocks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 import DiscountIfUContactNow from "@/components/home/discount/discountIfUContactNow";
 import WeAreAgency from "@/components/home/weAreAgency/weareagency";
-import WhatWeOfferYou from "@/components/home/whatWeOfferYou/whatWeOfferYou";
 import GradientCircle from "@/components/ui/gradient/Gradient";
+import WhatWeOfferYou from "@/components/ui/offerings/whatWeOfferYou";
+import Strengths from "@/components/ui/strengths/Strengths";
 
 export default function Home() {
   return (
-    <div>
-      <div className="mt-[300px] -ml-[200px] absolute w-[400px] h-[400px]">
+    <div className='w-[1312px] m-auto'>
+      <div className="mt-[300px] left-[-200px] absolute w-[400px] h-[400px]">
+        <GradientCircle />
+      </div>
+      <div className="mt-[180px] right-[-250px] absolute w-[500px] h-[500px]">
         <GradientCircle />
       </div>
       <WeAreAgency />
       <WhatWeOfferYou />
       <DiscountIfUContactNow />
+      <Strengths />
     </div>
   );
 }
