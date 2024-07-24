f790b5b0e0553185bf60892666e6da3ef270239d
EmilGlz <58591330+EmilGlz@users.noreply.github.com>
2024-07-24 11:46:05 -0400

    &apos fix

--- src/components/contact/Hero.tsx
+++ src/components/contact/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
         <div className='relative h-[600px] w-[1312px] m-auto'>
             <div className='w-[50%] ml-[100px]'>
                 <h1 className='font-semibold w-[100%] text-[60px] text-center'>
-                    Contact Us, Let's Solve Your Problems!
+                    Contact Us, Let &apos s Solve Your Problems!
                 </h1>
                 <p className='text-center w-[100%]'>We are here to quickly and effectively solve your IT needs. Get in touch with our expert team now.</p>
             </div>
