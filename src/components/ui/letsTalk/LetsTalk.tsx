2938b8a20d4f0b2020432f63f93a480e83e6f40a
EmilGlz <58591330+EmilGlz@users.noreply.github.com>
2024-07-24 11:40:38 -0400

    Added hover transition

--- src/components/ui/letsTalk/LetsTalk.tsx
+++ src/components/ui/letsTalk/LetsTalk.tsx
@@ -1,17 +1,18 @@
+'use client'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export interface LetsTalkConfig {
     text: string,
     buttonColor: string,
 }
-
 const LetsTalk = ({ config }: { config: LetsTalkConfig }) => {
     return (
-        <div className={`flex items-center space-x-2 p-4 rounded-2xl ${config.buttonColor}`} style={{ backgroundColor: config.buttonColor }} >
+        <Link href={'/contact'} className={`flex items-center space-x-2 p-4 rounded-2xl  transform transition-transform duration-300 hover:scale-105 ${config.buttonColor}`} style={{ backgroundColor: config.buttonColor }}>
             <span className='text-[16px] font-bold'>{config.text}</span>
-            <Image alt="" src="/lets-talk.png" width={20} height={20}/>
-        </div>
+            <Image alt="" src="/lets-talk.png" width={20} height={20} />
+        </Link>
     )
 }
 
